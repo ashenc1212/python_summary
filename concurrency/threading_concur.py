b3bacb8e32c3582771ab2ac1f2f2235845e0c672
shenche <shenche@momenta.ai>
2018-04-20 15:45:02 -0400

    notify

--- concurrency/threading_concur.py
+++ concurrency/threading_concur.py
@@ -2,16 +2,29 @@ import threading
 import time
 
 total = 0
-lock = threading.RLock()
+# lock = threading.RLock()
+cv = threading.Condition()
+R = 10000
 
-def foo(num):
+def increase(num):
     global total
-    lock.acquire()
-    for _ in range(1000000):
-        total = total + 1
-    lock.release()
+    for _ in range(R):
+        # print("increase", num, _)
+        with cv:
+            cv.wait_for(lambda: total < R/2)
+            total = total + 1
+            cv.notify_all()
 
-threads = [threading.Thread(target = foo, args = (i,)) for i in range(4)]
+def decrease(num):
+    global total
+    for _ in range(R):
+        # print("decrease", num, _)        
+        with cv:
+            cv.wait_for(lambda: total > 0)
+            total = total - 1
+            cv.notify_all()
+
+threads = [threading.Thread(target = increase, args = (i,)) for i in range(4)] + [threading.Thread(target = decrease, args = (i,)) for i in range(4)]
 [thr.start() for thr in threads]
 [thr.join() for thr in threads]
 print(total)
